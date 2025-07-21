9362375a5097bd9f31193bdf4dd1eabef935e4ef
Adrien Fryson <adrien.fryson@outlook.com>
2025-07-21 21:07:31 +0200

    feat(auth): add JWT-based authentication with user login and registration

---
+++ backend/src/main/java/com/promptoptimizer/backend/configuration/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.promptoptimizer.backend.configuration;
+
+import com.promptoptimizer.backend.service.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CustomUserDetails customUserDetails;
+    private final JwtUtils jwtUtils;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, PasswordEncoder passwordEncoder) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(customUserDetails).passwordEncoder(passwordEncoder);
+        return authenticationManagerBuilder.build();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/api/auth/*").permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(new JwtFilter(customUserDetails, jwtUtils), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+}
